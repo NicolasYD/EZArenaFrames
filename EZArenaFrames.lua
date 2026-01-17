19c10bf35fcc3cdb3bdb83ad2794f1f65ebf00b1
NicolasYD <147659516+NicolasYD@users.noreply.github.com>
2026-01-17 18:12:19 +0100

    Modified main file.

--- EZArenaFrames.lua
+++ EZArenaFrames.lua
@@ -1,6 +1,9 @@
 -- Create addon object and make it globally accessible
 local ADDON_NAME, NS = ...
 
+-- Library references
+local LibStub = LibStub
+
 local EZArenaFrames = LibStub("AceAddon-3.0"):NewAddon(
     ADDON_NAME,
     "AceEvent-3.0",
@@ -9,14 +12,9 @@ local EZArenaFrames = LibStub("AceAddon-3.0"):NewAddon(
 
 _G.EZArenaFrames = EZArenaFrames
 
--- Library references
-local LibStub = LibStub
-local AceDB = LibStub("AceDB-3.0")
-
 -- Localize WoW API functions
 local CreateFrame = CreateFrame
 local GetArenaOpponentSpec = GetArenaOpponentSpec
-local InCombatLockdown = InCombatLockdown
 
 -- Core lifecycle hooks
 function EZArenaFrames:OnInitialize()
@@ -30,19 +28,17 @@ function EZArenaFrames:OnInitialize()
             },
         },
     }, true)
-    self.db.RegisterCallback(self, "OnProfileChanged", "RefreshConfig")
-    self.db.RegisterCallback(self, "OnProfileCopied", "RefreshConfig")
-    self.db.RegisterCallback(self, "OnProfileReset", "RefreshConfig")
 end
 
 function EZArenaFrames:OnEnable()
     self:RegisterEvent("ARENA_PREP_OPPONENT_SPECIALIZATIONS")
+    self:RegisterEvent("PLAYER_ENTERING_WORLD")
 
     self:CreateAnchorFrames()
 end
 
 function EZArenaFrames:OnDisable()
-
+    self:UnregisterAllEvents()
 end
 
 -- Shared state flags
@@ -52,57 +48,72 @@ function EZArenaFrames:ARENA_PREP_OPPONENT_SPECIALIZATIONS()
     for i = 1, 3 do
         local specID = GetArenaOpponentSpec(i)
         if specID and specID > 0 then
-            local name = "EZAF_Arena" .. i .. "Anchor"
-            local frame = _G[name]
-
-            if frame then
-                frame:Show()
-            end
+            self:ShowAnchorFrames(i, true)
         end
     end
 end
 
-function EZArenaFrames:RefreshConfig()
-  -- would do some stuff here
+function EZArenaFrames:PLAYER_ENTERING_WORLD(event, isLogin, isReload)
+    for i = 1, 3 do
+        local specID = GetArenaOpponentSpec(i)
+        if specID and specID > 0 then
+            self:ShowAnchorFrames(i, true)
+        else
+            self:ShowAnchorFrames(i, true) -- for testing
+            -- self:ShowAnchorFrames(i, false)
+        end
+    end
 end
 
 -- Create anchor frames
 function EZArenaFrames:CreateAnchorFrames()
-    if InCombatLockdown() then return end
-
-    self.frames = self.frames or {}
+    self.anchorFrames = self.anchorFrames or {}
 
     for i = 1, 3 do
-        if not self.frames[i] then
+        if not self.anchorFrames[i] then
             local name = "EZAF_Arena" .. i .. "Anchor"
 
             local frame = _G[name] or CreateFrame("Frame", name, UIParent)
 
-            if not frame.bg then -- for testing
-                local bg = frame:CreateTexture(nil, "BACKGROUND")
-                bg:SetAllPoints()
-                bg:SetColorTexture(0, 0, 0, 1)
-                frame.bg = bg
+            if not frame.icon then -- for testing
+                local icon = frame:CreateTexture(nil, "OVERLAY")
+                frame.icon = icon
             end
 
             frame:Hide()
 
-            self.frames[i] = frame
+            self.anchorFrames[i] = frame
         end
     end
 
     self:StyleAnchorFrames()
 end
 
--- Update anchor frames
+-- Style anchor frames
 function EZArenaFrames:StyleAnchorFrames()
-    if InCombatLockdown() then return end
-
     local settings = self.db.profile.anchorFrames
 
-    for index, frame in ipairs(self.frames) do
+    for index, frame in ipairs(self.anchorFrames) do
         frame:ClearAllPoints()
-        frame:SetSize(settings.width, settings.height)
         frame:SetPoint("CENTER", UIParent, "CENTER", 0, -(index - 1) * settings.spacingY)
+        frame:SetSize(settings.width, settings.height) -- Invisible frames still need a size or the child frames are not shown
+
+        -- for testing
+        frame.icon:ClearAllPoints()
+        frame.icon:SetAllPoints()
+        frame.icon:SetTexture("Interface\\Icons\\INV_Misc_QuestionMark")
+    end
+end
+
+function EZArenaFrames:ShowAnchorFrames(index, show)
+    local name = "EZAF_Arena" .. index .. "Anchor"
+    local frame = _G[name]
+
+    if frame then
+        if show then
+            frame:Show()
+        else
+            frame:Hide()
+        end
     end
 end
