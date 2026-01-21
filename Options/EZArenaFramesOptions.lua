5685c3171ab6a715b14e02d8370c0e8adb68ef55
NicolasYD <147659516+NicolasYD@users.noreply.github.com>
2026-01-21 18:09:28 +0100

    Created some example settings.

--- Options/EZArenaFramesOptions.lua
+++ Options/EZArenaFramesOptions.lua
@@ -2,6 +2,8 @@ local ADDON_NAME, NS = ...
 local EZArenaFrames = NS.addon
 
 function EZArenaFrames:GetOptions()
+    local settings = self.db.profile
+
     return {
         type = "group",
         name = "EZ Arena Frames",
@@ -20,23 +22,55 @@ function EZArenaFrames:GetOptions()
                 type = "group",
                 name = "General",
                 args = {
-                    minimap = {
-                        type = "toggle",
-                        name = "Minimap Button",
-                        desc = "Show the minimap button.",
-                        width = "full",
+                    addon = {
+                        type = "group",
+                        name = "Addon",
+                        inline = true,
                         order = 10,
-                        get = function()
-                            return not EZArenaFrames.db.profile.general.addon.minimap.hide
-                        end,
-                        set = function(_, value)
-                            EZArenaFrames.db.profile.general.addon.minimap.hide = not value
-                            if value then
-                                EZArenaFrames.LDBIcon:Show("EZArenaFrames")
-                            else
-                                EZArenaFrames.LDBIcon:Hide("EZArenaFrames")
-                            end
-                        end,
+                        args = {
+                            minimap = {
+                                type = "toggle",
+                                name = "Minimap Button",
+                                desc = "Show the minimap button.",
+                                width = "full",
+                                order = 10,
+                                get = function()
+                                    return not EZArenaFrames.db.profile.general.addon.minimap.hide
+                                end,
+                                set = function(_, value)
+                                    EZArenaFrames.db.profile.general.addon.minimap.hide = not value
+                                    if value then
+                                        EZArenaFrames.LDBIcon:Show("EZArenaFrames")
+                                    else
+                                        EZArenaFrames.LDBIcon:Hide("EZArenaFrames")
+                                    end
+                                end,
+                            },
+                        },
+                    },
+                    size = {
+                        type = "group",
+                        name = "Size",
+                        inline = true,
+                        order = 20,
+                        args = {
+                            spacing = {
+                                type = "range",
+                                name = "Anchor Frames Spacing",
+                                desc = "Adjust the spacing between the anchor frames.",
+                                min = 0,
+                                max = 200,
+                                step = 1,
+                                get = function()
+                                    return settings.anchorFrames.spacingY
+                                end,
+                                set = function(_, value)
+                                    settings.anchorFrames.spacingY = value
+                                    EZArenaFrames:StyleAnchorFrames()
+                                    EZArenaFrames:StyleSecureAnchorFrames()
+                                end,
+                            },
+                        },
                     },
                 },
             },
--- Options/HealthBarOptions.lua
+++ Options/HealthBarOptions.lua
@@ -4,9 +4,52 @@ local EZArenaFrames = NS.addon
 local HealthBar = EZArenaFrames:GetModule("HealthBar")
 
 function HealthBar:GetOptions()
+    local settings = self.db.profile
+
     return {
         type = "group",
-        name = "HealthBar",
-        args = {},
+        name = "Health Bar",
+        args = {
+            size = {
+                type = "group",
+                name = "Size",
+                inline = true,
+                order = 1,
+                args = {
+                    width = {
+                        type = "range",
+                        name = "Width",
+                        desc = "Adjust the health bar width",
+                        min = settings.minWidth,
+                        max = settings.maxWidth,
+                        step = 1,
+                        get = function()
+                            return settings.width
+                        end,
+                        set = function(_, value)
+                            settings.width = value
+                            HealthBar:StyleHealthBar()
+                            HealthBar:StyleSecureButton()
+                        end,
+                    },
+                    height = {
+                        type = "range",
+                        name = "Height",
+                        desc = "Adjust the health bar height",
+                        min = settings.minHeight,
+                        max = settings.maxHeight,
+                        step = 1,
+                        get = function()
+                            return settings.height
+                        end,
+                        set = function(_, value)
+                            settings.height = value
+                            HealthBar:StyleHealthBar()
+                            HealthBar:StyleSecureButton()
+                        end,
+                    },
+                },
+            },
+        },
     }
 end
