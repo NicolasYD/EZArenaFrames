cc96c71bd5391ef48ba115648a78cc49c6485e70
NicolasYD <147659516+NicolasYD@users.noreply.github.com>
2026-01-24 23:04:43 +0100

    Added an option to choose in what direction the DRs should grow.

--- Options/DRTrackerOptions.lua
+++ Options/DRTrackerOptions.lua
@@ -11,7 +11,30 @@ function DRTracker:GetOptions(moduleOrder)
         name = "DR Tracker",
         order = moduleOrder,
         args = {
-
+            widget = {
+                type = "group",
+                name = "Widget",
+                inline = true,
+                order = 1,
+                args = {
+                    grow = {
+                        type = "select",
+                        name = "Grow",
+                        desc = "Choose in which directions the DR icons should grow.",
+                        values = {
+                            ["LEFT"] = "Left",
+                            ["RIGHT"] = "Right",
+                        },
+                        get = function(info)
+                            return settings.grow
+                        end,
+                        set = function(info, value)
+                            settings.grow = value
+                            DRTracker:StyleDRFrames()
+                        end,
+                    },
+                }
+            }
         },
     }
 end
