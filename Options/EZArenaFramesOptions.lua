f17f6302fc70305b277ebd14c91bdf9a58667950
NicolasYD <147659516+NicolasYD@users.noreply.github.com>
2026-03-13 12:17:00 +0100

    Added option to lock the position of the arena frames.

--- Options/EZArenaFramesOptions.lua
+++ Options/EZArenaFramesOptions.lua
@@ -37,10 +37,10 @@ function EZArenaFrames:GetOptions()
                                 width = "full",
                                 order = 10,
                                 get = function()
-                                    return not EZArenaFrames.db.profile.general.addon.minimap.hide
+                                    return not settings.general.addon.minimap.hide
                                 end,
                                 set = function(_, value)
-                                    EZArenaFrames.db.profile.general.addon.minimap.hide = not value
+                                    settings.general.addon.minimap.hide = not value
                                     if value then
                                         EZArenaFrames.LDBIcon:Show("EZArenaFrames")
                                     else
@@ -52,17 +52,32 @@ function EZArenaFrames:GetOptions()
                     },
                     size = {
                         type = "group",
-                        name = "Size",
+                        name = "Arena Frames",
                         inline = true,
                         order = 20,
                         args = {
+                            positionLocked = {
+                                type = "toggle",
+                                name = "Lock Position",
+                                desc = "Lock the position of the arena frames.",
+                                width = "full",
+                                order = 1,
+                                get = function()
+                                    return settings.containerFrame.locked
+                                end,
+                                set = function(_, value)
+                                    settings.containerFrame.locked = value
+                                    EZArenaFrames:StyleContainerFrame()
+                                end,
+                            },
                             spacing = {
                                 type = "range",
-                                name = "Anchor Frames Spacing",
+                                name = "Spacing",
                                 desc = "Adjust the spacing between the anchor frames.",
                                 min = 0,
                                 max = 200,
                                 step = 1,
+                                order = 2,
                                 get = function()
                                     return settings.anchorFrames.spacingY
                                 end,
